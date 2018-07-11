34437fc18e9aac5accd739996537a983534d2861
rajeevkumarak <41045582+rajeevkumarak@users.noreply.github.com>
2018-07-11 11:42:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>Developing first develop hope it works fine PHP Application</h1>
+<h1>manual approval process inbuild with this php code delopy</h1>
 
 <?php
 echo "version 1.0";
